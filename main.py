9cdd1fcc0d1feff5dba49ac0da5f6ac4839d492d
BGPCS <90006794+BGPCS@users.noreply.github.com>
2023-05-11 08:10:36 -0700

    Update main.py
    Added a Unit Test.

--- main.py
+++ main.py
@@ -1,6 +1,67 @@
 import pygame
 import random
+import unittest
+from unittest.mock import patch
 
+class TestPianoTilesGame(unittest.TestCase):
+
+    def setUp(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((400, 600))
+        self.clock = pygame.time.Clock()
+        self.note_speed = 20
+        self.note_size = 100
+        self.note_colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0)]
+        self.note_paths = ['a', 's', 'd', 'f']
+        self.pressed_paths = set()
+        self.falling_notes = []
+        self.score = 0
+        self.title_font = pygame.font.Font(None, 72)
+        self.subtitle_font = pygame.font.Font(None, 36)
+        self.score_font = pygame.font.Font(None, 36)
+
+    def tearDown(self):
+        pygame.quit()
+
+    def test_create_note(self):
+        with patch('random.choice') as mock_choice:
+            mock_choice.side_effect = [('a', (255, 0, 0)), (2, (0, 0, 255)), (1, (0, 255, 0))]
+            note = create_note()
+            self.assertEqual(note['path'], 'a')
+            self.assertEqual(note['color'], (255, 0, 0))
+            self.assertEqual(note['rect'].x, 0)
+            self.assertEqual(note['rect'].y, -self.note_size / 4)
+            self.assertEqual(note['rect'].width, self.note_size)
+            self.assertEqual(note['rect'].height, self.note_size)
+
+            note = create_note()
+            self.assertEqual(note['path'], 'd')
+            self.assertEqual(note['color'], (0, 0, 255))
+            self.assertEqual(note['rect'].x, 2 * (self.screen.get_width() // len(self.note_paths)))
+            self.assertEqual(note['rect'].y, -self.note_size / 4)
+            self.assertEqual(note['rect'].width, self.note_size)
+            self.assertEqual(note['rect'].height, self.note_size)
+
+            note = create_note()
+            self.assertEqual(note['path'], 's')
+            self.assertEqual(note['color'], (0, 255, 0))
+            self.assertEqual(note['rect'].x, 1 * (self.screen.get_width() // len(self.note_paths)))
+            self.assertEqual(note['rect'].y, -self.note_size / 4)
+            self.assertEqual(note['rect'].width, self.note_size)
+            self.assertEqual(note['rect'].height, self.note_size)
+
+    def test_move_notes(self):
+        note = {'path': 'a', 'color': (255, 0, 0), 'rect': pygame.Rect(0, 0, self.note_size, self.note_size)}
+        self.falling_notes = [note]
+        move_notes()
+        self.assertEqual(note['rect'].y, self.note_speed)
+
+        note = {'path': 'd', 'color': (0, 0, 255),
+                'rect': pygame.Rect(3 * (self.screen.get_width() // len(self.note_paths)), 0, self.note_size,
+                                    self.note_size)}
+        self.falling_notes = [note]
+        move_notes()
+        self.assertEqual(note['rect'].y, self.note_speed)
 # Initialize Pygame
 pygame.init()
 
@@ -69,8 +130,9 @@ def draw_zones():
         screen.blit(zone_text, (zone_x + zone_width // 2 - zone_text.get_width() // 2, zone_y + zone_height // 2 - zone_text.get_height() // 2))
 
 def show_title_screen():
-    title_text = title_font.render('Note Catcher', True, white)
-    subtitle_text = subtitle_font.render('Press any key to start', True, white)
+    title_text = title_font.render('Piano Tiles', True, white)
+    subtitle_text = subtitle_font.render('Press A/S/D/F key to start', True, white)
+
     title_x = screen_width // 2 - title_text.get_width() // 2
     title_y = screen_height // 3
     subtitle_x = screen_width // 2 - subtitle_text.get_width() // 2
@@ -86,7 +148,9 @@ def show_title_screen():
                 waiting = False
 # Game loop
 clock = pygame.time.Clock()
+TestPianoTilesGame()
 show_title_screen()
+
 game_running = True
 
 while game_running:
@@ -119,7 +183,7 @@ while game_running:
     pygame.display.flip()
 
     # Limit the frame rate
-    clock.tick(30)
+    clock.tick(45)
 
 # Clean up
 pygame.quit()
